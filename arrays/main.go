e9ef3348d8fe7dc7f68f684f7e96e7b6a7c3eebb
= <yashas1145@proton.me>
2026-01-09 08:34:53 +0530

    refactor move zeroes problem

--- arrays/main.go
+++ arrays/main.go
@@ -1,13 +1,5 @@
 package main
 
-import "fmt"
-
 func main() {
-	nums := []int{0, 1, 0, 3, 12}
-	moveZeroes(nums)
-
-	for i := range nums {
-		fmt.Printf("%d ", nums[i])
-	}
-	fmt.Println()
+	moveZeroesDriver()
 }
--- arrays/moveZeroes.go
+++ arrays/moveZeroes.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 func moveZeroes(nums []int) {
 	i, j := 0, 0
 	for i = 0; i < len(nums); i++ {
@@ -13,4 +15,14 @@ func moveZeroes(nums []int) {
 		i++
 		j++
 	}
+}
+
+func moveZeroesDriver() {
+	nums := []int{0, 1, 0, 3, 12}
+	moveZeroes(nums)
+
+	for i := range nums {
+		fmt.Printf("%d ", nums[i])
+	}
+	fmt.Println()
 }
