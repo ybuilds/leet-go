74cb8be593e05be9be25670ea43c6196af2f87a9
= <yashas1145@proton.me>
2026-01-10 08:22:09 +0530

    refactor sell stock 2 problem

--- arrays/sellStock2.go
+++ arrays/sellStock2.go
@@ -2,38 +2,29 @@ package main
 
 import "fmt"
 
-func ascendingSorted(arr []int) bool {
-	for i := 1; i < len(arr); i++ {
-		if arr[i] < arr[i-1] {
-			return false
-		}
-	}
-	return true
-}
-
-func descendingSorted(arr []int) bool {
-	for i := len(arr) - 1; i > 0; i-- {
-		if arr[i] < arr[i-1] {
-			return false
-		}
-	}
-	return true
-}
-
 func maxProfit2(prices []int) int {
-	if ascendingSorted(prices) {
-		return prices[len(prices)-1] - prices[0]
+	i, profit := 0, 0
+	valley, peak := 0, 0
+
+	for i < len(prices)-1 {
+		for i < len(prices)-1 && prices[i] >= prices[i+1] {
+			i++
+		}
+		valley = prices[i]
+
+		for i < len(prices)-1 && prices[i] <= prices[i+1] {
+			i++
+		}
+		peak = prices[i]
+
+		profit += peak - valley
 	}
-	if descendingSorted(prices) {
-		return 0
-	}
-	profit := 0
 
 	return profit
 }
 
 func maxProfit2Driver() {
-	prices := []int{7, 6, 4, 3, 1}
+	prices := []int{7, 1, 5, 3, 6, 4}
 	profit := maxProfit2(prices)
 	fmt.Println(profit)
 }
